21183e8edc66d5494c3f5b21b7f1fb85ebcf6a2a
Luigui de A <luisguilhermepdea@gmail.com>
2021-10-19 07:56:50 -0300

    Fix problems

--- nlp_cleaner/nlp_cleaner.py
+++ nlp_cleaner/nlp_cleaner.py
@@ -1,4 +1,4 @@
-from string_formatter import StringFormatter
+from nlp_cleaner.string_formatter import StringFormatter
 
 
 class NlpCleaner:
@@ -7,10 +7,17 @@ class NlpCleaner:
         self.string_formatter = StringFormatter()
 
     def remove_all_twitter(self, text: str) -> str:
+        print(text)
         new_text = self.string_formatter.replace_html(text)
+        print(new_text)
         new_text = self.string_formatter.replace_url(new_text)
+        print(new_text)
         new_text = self.string_formatter.replace_username_twitter(new_text)
+        print(new_text)
         new_text = self.string_formatter.replace_emoji(new_text)
+        print(new_text)
         new_text = self.string_formatter.replace_flag(new_text)
+        print(new_text)
         new_text = self.string_formatter.replace_hashtag_twitter(new_text)
+        print(new_text)
         return self.string_formatter.remove_punctuation(new_text)
--- nlp_cleaner/string_formatter.py
+++ nlp_cleaner/string_formatter.py
@@ -5,23 +5,23 @@ import string
 
 class StringFormatter:
 
-    def replace_url(text, value=''):
+    def replace_url(self, text, value=''):
         return re.sub(r'https?://\S+|www\.\S+', value, text)
 
-    def replace_html(text, value=''):
+    def replace_html(self, text, value=''):
         return re.sub(r'(</?\w*\s*/?>)', value, text)
 
-    def replace_emoji(text, value=''):
-        return ''.join(c for c in text if c in emoji.UNICODE_EMOJI)
+    def replace_emoji(self, text, value=''):
+        return ''.join(c for c in text if c not in emoji.UNICODE_EMOJI)
 
-    def replace_flag(text, value=''):
+    def replace_flag(self, text, value=''):
         return re.sub(u'[\U0001F1E6-\U0001F1FF]', value, text) 
 
-    def replace_username_twitter(text, value=''):
+    def replace_username_twitter(self, text, value=''):
         return re.sub(r'@[a-zA-Z0-9_]+', value,  text)
 
-    def replace_hashtag_twitter(text, value=''):
+    def replace_hashtag_twitter(self, text, value=''):
         return re.sub(r'#[a-zA-Z0-9_]+', value, text)
 
-    def remove_punctuation(text):
+    def remove_punctuation(self, text):
         return text.translate(str.maketrans('', '', string.punctuation))
